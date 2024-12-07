8fbf3c58a5470de34e4fce59cf779497e2f17fdf
thewoowon <thewoowon@naver.com>
2024-12-07 11:25:29 +0900

    feat: erase classify api

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 
 from app.api.v1.endpoints import items
-from app.api.v1.endpoints import classify
+from app.api.v1.endpoints import items
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
-api_router.include_router(classify.router, prefix="/classify", tags=["classify"])
+api_router.include_router(items.router, prefix="/socket", tags=["socket"])
