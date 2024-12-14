5cda18e6fce3027b817ec8d097aaecb283ac3061
thewoowon <thewoowon@naver.com>
2024-12-14 13:47:24 +0900

    feat: schemas init

---
+++ app/schemas/user.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserResponse(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
