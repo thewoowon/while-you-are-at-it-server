8a26ff8b1924539de44e00904f8c0f48ad932da0
thewoowon <thewoowon@naver.com>
2024-12-29 14:16:10 +0900

    feat: enspoints changed

--- app/api/v1/endpoints/article.py
+++ app/api/v1/endpoints/article.py
@@ -9,21 +9,19 @@ from typing import List
 router = APIRouter()
 
 
-@router.post("/", response_model=ArticleResponse)
-def create(article: ArticleCreate, db: Session = Depends(get_db), user_id: int = Depends(get_current_user)):
-    return create_article(db=db, article=article, user_id=user_id)
-
-
-# 아티클 조회 -> 이게 user_id를 받을 필요가 있나?
-# 지역으로 조회할 수 있게 하려면 어떻게 해야할까?
-@router.get("/{article_id}", response_model=ArticleResponse)
+@router.get("/one", response_model=ArticleResponse)
 def read_one(article_id: int, db: Session = Depends(get_db)):
     return get_article_by_id(db=db, article_id=article_id)
 
 
-@router.get("/", response_model=List[ArticleResponse])
+@router.get("/all", response_model=List[ArticleResponse])
 def read_all(user_id: int = Depends(get_current_user), db: Session = Depends(get_db)):
     return get_article_by_user_id(db=db, user_id=user_id)
+
+
+@router.post("/", response_model=ArticleResponse)
+def create(article: ArticleCreate, db: Session = Depends(get_db), user_id: int = Depends(get_current_user)):
+    return create_article(db=db, article=article, user_id=user_id)
 
 
 @router.put("/{article_id}", response_model=ArticleResponse)
--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -1,9 +1,11 @@
 
 from fastapi import APIRouter, Request, Depends
-from app.services.auth_service import google_auth, naver_auth, kakao_auth
-from app.schemas.auth import AuthRequest
+from fastapi.responses import JSONResponse
+from app.core.security import get_current_user
+from app.services.auth_service import google_auth, naver_auth, kakao_auth, refresh_token_func
 from app.dependencies import get_db
 from sqlalchemy.orm import Session
+# from fastapi import FastAPI, Header
 
 router = APIRouter()
 
@@ -21,3 +23,22 @@ def naver(access_token: str):
 @router.post("/kakao")
 def kakao(access_token: str):
     return kakao_auth(access_token)
+
+
+# /auth/validate-token -> 토큰 유효성 검사
+# /auth/refresh-token -> 토큰 갱신
+# /auth/logout -> 로그아웃
+
+@router.get("/validate-token")
+def validate_token(user_id: int = Depends(get_current_user)):
+    return JSONResponse(content={"message": "Token is valid", "user_id": user_id}, status_code=200)
+
+
+@router.post("/refresh-token")
+def refresh_token(request: Request, db: Session = Depends(get_db)):
+    return refresh_token_func(request, db)
+
+
+@router.post("/logout")
+def logout():
+    pass
