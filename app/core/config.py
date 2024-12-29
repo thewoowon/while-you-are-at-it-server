56451d4704710f1b1706db6c28823c1ca231546a
thewoowon <thewoowon@naver.com>
2024-12-29 14:15:52 +0900

    feat: token authorization process edited

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "WhileYouAreAtIt API"
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    DEBUG: bool = True
 
     @field_validator("BACKEND_CORS_ORIGINS")
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
--- app/core/security.py
+++ app/core/security.py
@@ -1,24 +1,49 @@
-# core/security.py
+import logging
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+import jwt
+from settings import (
+    JWT_SECRET_KEY,
+    JWT_ALGORITHM,
+)
+
+# 로깅 설정
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 def decode_token(token: str):
-    # JWT 디코딩 로직 (예: PyJWT 사용)
-    # 토큰에서 user_id를 추출하는 함수
-    # 여기에 토큰 유효성 검사 포함
-    return {"user_id": "example_user_id"}  # 예시: 디코딩 후 반환값
+    try:
+        logger.info(f"Decoding token: {token}")
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        user_id: str = payload.get("user_id")
+        if user_id is None:
+            logger.warning("Invalid token: Missing user_id")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid token",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+        logger.info(f"Token is valid. User ID: {user_id}")
+        return user_id
+    except jwt.ExpiredSignatureError:
+        logger.warning("Token has expired")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except jwt.InvalidTokenError:
+        logger.error("Invalid token")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
 
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    payload = decode_token(token)
-    if not payload or "user_id" not in payload:
-        raise credentials_exception
-    return payload["user_id"]
+    logger.info(f"Received token: {token}")
+    return decode_token(token)
